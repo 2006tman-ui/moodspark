95c631c99ad852f263eb37324b3de67d229096b6
2006tman-ui <2006tman@gmail.com>
2026-03-27 11:43:50 -0400

    Delete MoodSpark directory

--- MoodSpark/manifest.js
+++
@@ -1,8 +0,0 @@
-const manifest = {
-  "name": "MoodSpark",
-  "short_name": "MoodSpark",
-  "start_url": "index.html",
-  "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#4a90e2"
-};
